7e65692c5fa2c8fa03dd16940017fb9746129121
Dhruv224224 <dhruvmaheshwari18vk@gmail.com>
2024-09-28 13:30:45 +0530

    Create DAY 1 - AVERAGE.py

---
+++ DAY 1 - AVERAGE.py
@@ -0,0 +1,8 @@
+# Write a python program to find an average of two numbers entered by the user.
+
+a  = int(input('enter the first number : ' ))
+
+b = int(input('enter the second number : '))
+
+print('the average of a and b is' , (a+b)/2)
+
