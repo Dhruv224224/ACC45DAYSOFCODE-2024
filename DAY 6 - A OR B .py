bf6d4b87c46f3bd7784d8c139ba982ddc01936de
Dhruv224224 <dhruvmaheshwari18vk@gmail.com>
2024-10-03 21:27:13 +0530

    Create DAY 6 - A OR B .py

---
+++ DAY 6 - A OR B .py
@@ -0,0 +1,43 @@
+# Test case 1
+X = 10
+Y = 20
+score_A_first = (500 - 2 * X) + (1000 - 4 * (X + Y))
+score_B_first = (1000 - 4 * Y) + (500 - 2 * (X + Y))
+
+if score_A_first > score_B_first:
+    print(score_A_first)
+else:
+    print(score_B_first)
+
+# Test case 2
+X = 8
+Y = 40
+score_A_first = (500 - 2 * X) + (1000 - 4 * (X + Y))
+score_B_first = (1000 - 4 * Y) + (500 - 2 * (X + Y))
+
+if score_A_first > score_B_first:
+    print(score_A_first)
+else:
+    print(score_B_first)
+
+    # Test case 3
+X = 15
+Y = 15
+score_A_first = (500 - 2 * X) + (1000 - 4 * (X + Y))
+score_B_first = (1000 - 4 * Y) + (500 - 2 * (X + Y))
+
+if score_A_first > score_B_first:
+    print(score_A_first)
+else:
+    print(score_B_first)
+
+    # Test case 4
+X = 20
+Y = 10
+score_A_first = (500 - 2 * X) + (1000 - 4 * (X + Y))
+score_B_first = (1000 - 4 * Y) + (500 - 2 * (X + Y))
+
+if score_A_first > score_B_first:
+    print(score_A_first)
+else:
+    print(score_B_first)
