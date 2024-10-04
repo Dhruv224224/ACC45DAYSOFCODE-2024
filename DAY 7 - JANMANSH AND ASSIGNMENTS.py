768077e40e05c1b1233e701c7b1211e898df8fad
Dhruv224224 <dhruvmaheshwari18vk@gmail.com>
2024-10-04 21:21:16 +0530

    Create DAY 7 - JANMANSH AND ASSIGNMENTS.py

---
+++ DAY 7 - JANMANSH AND ASSIGNMENTS.py
@@ -0,0 +1,10 @@
+# A simple list of test cases (start times)
+test_cases = [ 7, 9]
+
+# Check if each start time is <= 7 and print the result
+for X in test_cases:
+    if X <= 7:
+        print("Yes")
+    else:
+        print("No")
+
